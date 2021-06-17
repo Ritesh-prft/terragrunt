ee884d2cc17156d66411ac838240cf91675a3029
Ritesh Khadke <ritesh.khadke@perficient.com>
2021-06-17 17:39:40 +0530

    a

--- modules/ec2/instance.tf
+++ modules/ec2/instance.tf
@@ -1,9 +1,29 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.type
-  subnet_id = var.subnet_id
+  subnet_id = "${aws_subnet.web-subnet.id}"
 
   tags = {
     Name = "jenkins-vm"
   }
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+
+resource "aws_vpc" "terra-demo-vpc" {
+    cidr_block = "192.168.0.0/16"
+    tags = {
+      "Name" = "jenkins-terragrunt-vpc"
+    }
+}
+resource "aws_subnet" "web-subnet" {
+    vpc_id = "${aws_vpc.terra-demo-vpc.id}"
+    cidr_block = "192.168.0.0/24"
+    availability_zone = "ap-south-1a"
+    tags = {
+      Name = "jenkins-terragrunt-websubnet"
+    }
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -3,5 +3,3 @@ variable "ami_id" {}
 variable "type"{
     default = "t2.micro"
 }
-
-variable "subnet_id"{}
