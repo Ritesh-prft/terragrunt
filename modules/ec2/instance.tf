a14ade0b0c8112885a0498966582fe5b3c255b91
Ritesh Khadke <ritesh.khadke@perficient.com>
2021-06-22 13:54:32 +0530

    aa

--- modules/ec2/instance.tf
+++ modules/ec2/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
-  instance_type = var.type
+  instance_type = var.instance_type
   subnet_id = "${aws_subnet.web-subnet.id}"
 
   tags = {
@@ -9,19 +9,19 @@ resource "aws_instance" "web" {
 }
 
 provider "aws" {
-    region = "ap-south-1"
+    region = var.region
 }
 
 
 resource "aws_vpc" "terra-demo-vpc" {
-    cidr_block = "192.168.0.0/16"
+    cidr_block = var.vpc_cidr
     tags = {
       "Name" = "jenkins-terragrunt-vpc"
     }
 }
 resource "aws_subnet" "web-subnet" {
     vpc_id = "${aws_vpc.terra-demo-vpc.id}"
-    cidr_block = "192.168.0.0/24"
+    cidr_block = var.subnet_cidr
     availability_zone = "ap-south-1a"
     tags = {
       Name = "jenkins-terragrunt-websubnet"
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,4 +1,7 @@
 variable "ami_id" {}
 
-variable "instance_type"{
-}
+variable "instance_type"{}
+
+variable "region"{}
+variable "vpc_cidr"{}
+variable "subnet_cidr"{}
